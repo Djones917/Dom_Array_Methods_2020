0fe755306b5e0c7ff602cd3f44cfbd99890203f4
Derek <nightwing917@hotmail.com>
2020-06-27 19:34:39 -0600

    format money working

--- app.js
+++ app.js
@@ -47,11 +47,15 @@ function updateDom(providedData = data) {
    providedData.forEach(item => {
       const element = document.createElement('div');
       element.classList.add('person');
-      element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+      element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
       main.appendChild(element);
    });
 }
 
 
-// Form Money
+// Format number as Money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
 
